e340059719433583ccaf0f6f31ff3805547063f0
Kodex <205018495+KODEX-DEV-GIT@users.noreply.github.com>
2026-02-19 12:11:52 +0500

    added import

--- RSLoadingView/Classes/RSLoadingContainerView.swift
+++ RSLoadingView/Classes/RSLoadingContainerView.swift
@@ -1,5 +1,5 @@
 import Foundation
-
+import UIKit
 internal class RSLoadingContainerView: UIView {
   let logger = RSLogger(tag: "RSLoadingContainerView")
   internal var loadingView: RSLoadingView!
