e40a553023a5801e3513918e5b71997748245e36
Kodex <205018495+KODEX-DEV-GIT@users.noreply.github.com>
2026-02-19 12:32:36 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "RSLoadingView",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v16)
     ],
     products: [
         .library(name: "RSLoadingView", targets: ["RSLoadingView"])
